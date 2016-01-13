a26c74f28730ab1eb8a7305e82030d8e49f2b6d9
Admin <srkmenon93rk@gmail.com>
2016-01-13 12:51:26 +0530

    PieChart and Cleaner IOOperations done

---
+++ Directree/Logic/Cleaner/IOOperations.cs
@@ -0,0 +1,193 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Directree.Logic.Cleaner
+{
+    class IOOperations
+    {
+        /// <summary>
+        /// Deletes all files of specified extensions in a directory (and its subdirectories)
+        /// </summary>
+        /// <param name="sourceRoot">   The root directory to delete files from</param>
+        /// <param name="extList">      The Extensions to delete. ******** KEEP NULL FOR ALL FILES ********</param>
+        /// <param name="includeSub">   Recursively delete files in all sub directories</param>
+        public static void Delete(string sourceRoot, List<string> extList, bool includeSub)
+        {
+            if (includeSub)
+            {
+                // implementing a queue based approach to avoid recursion
+                Queue<string> folderQueue = new Queue<string>();
+                folderQueue.Enqueue(sourceRoot);
+
+                DirectoryInfo directoryInfo;
+                while (folderQueue.Count > 0)
+                {
+                    string currFolder = folderQueue.Dequeue();
+                    clearDirectory(currFolder, extList);
+
+                    directoryInfo = new DirectoryInfo(currFolder);
+                    foreach (DirectoryInfo dir in directoryInfo.GetDirectories())
+                    {
+                        folderQueue.Enqueue(dir.FullName);
+                    }
+                }
+            }
+            else
+            {
+                clearDirectory(sourceRoot, extList);
+            }
+
+            deleteEmptyDirectories(sourceRoot);
+        }
+
+        /// <summary>
+        /// delete all files of specified extentions in a directory
+        /// </summary>
+        /// <param name="directory">    current directory</param>
+        /// <param name="extList">      Extenstions of files that should be erased</param>
+        private static void clearDirectory(string directory, List<string> extList)
+        {
+            DirectoryInfo di = new DirectoryInfo(directory);
+
+            foreach (FileInfo file in di.GetFiles())
+            {
+                if (extList.Count == 0 || extList.Contains(file.Extension.ToLower()))
+                    file.Delete();
+            }
+        }
+
+        /// <summary>
+        /// delete all empty directories after an action
+        /// </summary>
+        /// <param name="directory"> directory</param>
+        private static void deleteEmptyDirectories(string directory)
+        {
+            // implementing a stack based approach to avoid recursion
+            Stack<string> folderStack = new Stack<string>();
+
+            // get to the deepest folder first, populating the stack simultaneously
+            Queue<string> folderQueue = new Queue<string>();
+            folderQueue.Enqueue(directory);
+
+            DirectoryInfo directoryInfo;
+            while (folderQueue.Count > 0)
+            {
+                string currFolder = folderQueue.Dequeue();
+                folderStack.Push(currFolder);
+
+                directoryInfo = new DirectoryInfo(currFolder);
+                foreach (DirectoryInfo dir in directoryInfo.GetDirectories())
+                {
+                    folderQueue.Enqueue(dir.FullName);
+                }
+            }
+
+            // here the stack is filled with folders such that the deepest folder is on stack top
+            //now delete all folders by iteratively popping
+            while (folderStack.Count > 0)
+            {
+                DirectoryInfo dir = new DirectoryInfo(folderStack.Pop());
+
+                if (dir.GetDirectories().Length == 0 && dir.GetFiles().Length == 0)
+                    dir.Delete();
+            }
+        }
+
+        /// <summary>
+        /// Move all files in a directory that have a specified extentions
+        /// </summary>
+        /// <param name="sourceRoot"> the root directory of source</param>
+        /// <param name="destinationRoot"> the root directory of destination</param>
+        /// <param name="extList">      The Extensions to move. ******** KEEP NULL FOR ALL FILES ********</param>
+        /// <param name="includeSub">   Recursively move files in all sub directories</param>
+        public static void Move(string sourceRoot, string destinationRoot, List<string> extList, bool includeSub)
+        {
+            List<MoveItem> moveList = new List<MoveItem>();
+
+            if (includeSub)
+            {
+                // implementing a queue based approach to avoid recursion
+                Queue<string> folderQueue = new Queue<string>();
+                folderQueue.Enqueue(sourceRoot);
+
+                DirectoryInfo directoryInfo;
+                while (folderQueue.Count > 0)
+                {
+                    string currFolder = folderQueue.Dequeue();
+                    moveList.AddRange(moveDirectory(currFolder, extList, sourceRoot, destinationRoot));
+
+                    directoryInfo = new DirectoryInfo(currFolder);
+                    foreach (DirectoryInfo dir in directoryInfo.GetDirectories())
+                    {
+                        folderQueue.Enqueue(dir.FullName);
+                    }
+                }
+            }
+            else
+            {
+                moveList.AddRange(moveDirectory(sourceRoot, extList, sourceRoot, destinationRoot));
+            }
+
+            foreach (MoveItem item in moveList)
+            {
+                item.move();
+            }
+
+            deleteEmptyDirectories(sourceRoot);
+        }
+
+        /// <summary>
+        /// returns all files to move and its destination as a list of MoveItems
+        /// this is implemented to resolve the problem of duplicate and infinite moving when source is the ancestor of destination directory or vice versa
+        /// </summary>
+        /// 
+        /// <param name="sourceDirectory">current source directory</param>
+        /// <param name="extList">the extension list of files that should be moved</param>
+        /// <param name="sourceRoot"> the root source directory</param>
+        /// <param name="destinationRoot"> the root destination directory</param>
+        /// 
+        /// <returns>List of all Moveitems that should be moved of this particular sourceDirectory</returns>
+        private static List<MoveItem> moveDirectory(string sourceDirectory, List<string> extList, string sourceRoot, string destinationRoot)
+        {
+            List<MoveItem> list = new List<MoveItem>();
+
+            DirectoryInfo di = new DirectoryInfo(sourceDirectory);
+
+            string destinationDirectory;
+            foreach (FileInfo file in di.GetFiles())
+            {
+                if (extList.Count == 0 || extList.Contains(file.Extension.ToLower()))
+                {
+                    destinationDirectory = file.DirectoryName.Replace(sourceRoot, destinationRoot);
+
+                    if (!Directory.Exists(destinationDirectory))
+                        Directory.CreateDirectory(destinationDirectory);
+
+                    list.Add(new MoveItem(file, file.FullName.Replace(sourceRoot, destinationRoot)));
+                }
+            }
+
+            return list;
+        }
+
+        /// <summary>
+        /// A structure class for all the (source)files that should be moved to (destination) location 
+        /// </summary>
+        class MoveItem
+        {
+            private FileInfo source;
+            private string destination;
+
+            public MoveItem(FileInfo source, string destination)
+            {
+                this.source = source;
+                this.destination = destination;
+            }
+
+            public void move()
+            {
+                source.MoveTo(destination);
+            }
+        }
+    }
+}
---
+++ Directree/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Collections.ObjectModel;
+using Directree.WPFPieChart.Shapes;
+using Directree.Logic.Analytics;
+
+namespace Directree
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //private ObservableCollection<AssetClass> classes;/////////////////
+        //private ObservableCollection<AssetClass> classes1;////////////////
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //classes = new ObservableCollection<AssetClass>(AssetClass.ConstructTestData());///////////
+            //piePlotterC.DataContext = classes;////////////
+
+            //classes1 = new ObservableCollection<AssetClass>(AssetClass.ConstructTestData1());/////////////
+            //piePlotterD.DataContext = classes1;/////////////////
+        }
+
+        /// <summary>
+        /// Close the entire application
+        /// </summary>
+        private void closeApp()
+        {
+            App.Current.Shutdown();
+        }
+
+        ///////////////////////// Events ////////////////////////////////
+        private void menu_file_exit_Click(object sender, RoutedEventArgs e)
+        {
+            closeApp();
+        }
+    }
+}
